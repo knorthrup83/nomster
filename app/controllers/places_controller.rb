515fcbace2faab1a3ec2b07eb930871cc8767233
Kevin Northrup <knorthrup83@gmail.com>
2019-07-28 18:56:00 +0000

    Hooked up new entry page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,4 +6,13 @@ class PlacesController < ApplicationController
   def new
     @place = Place.new
   end
+
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
 end
