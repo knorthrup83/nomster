d1518e94cecbdcdc8f1d2accf583e53c085af0a9
Kevin Northrup <knorthrup83@gmail.com>
2019-07-30 22:09:36 +0000

    Connected databases, added email address to entries

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,6 @@
 class PlacesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
     @places = Place.all.page(params[:page]).per(5)
   end
@@ -8,9 +10,11 @@ class PlacesController < ApplicationController
   end
 
   def create
-    Place.create(place_params)
+    current_user.places.create(place_params)
     redirect_to root_path
   end
+
+  private
 
   def place_params
     params.require(:place).permit(:name, :description, :address)
