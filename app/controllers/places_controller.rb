081535fc41e068853d8e3548e7d673126d574f2d
Kevin Northrup <knorthrup83@gmail.com>
2019-08-03 20:18:59 +0000

    Secured delete feature and added validation to new place names

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,12 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -31,7 +35,11 @@ class PlacesController < ApplicationController
       return render plain: 'Not Allowed', status: :forbidden
     end
     @place.update_attributes(place_params)
-    redirect_to root_path
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
