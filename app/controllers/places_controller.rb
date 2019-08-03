ef6157c63a14fa4679387cf6e5ede5fdf1cfb9d3
Kevin Northrup <knorthrup83@gmail.com>
2019-08-03 19:58:31 +0000

    Restricted access to delete/destroy function

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @places = Place.all.page(params[:page]).per(5)
@@ -36,6 +36,9 @@ class PlacesController < ApplicationController
 
   def destroy
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
     @place.destroy
     redirect_to root_path
   end
